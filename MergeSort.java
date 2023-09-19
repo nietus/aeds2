5168e68b95f4356e594c69547a1a3a4415e2adef
Antonio Neto <antonionetodev@yahoo.com>
2023-09-19 18:28:53 -0300

    add: quicksort

--- MergeSort.java
+++ MergeSort.java
@@ -16,7 +16,7 @@ public class MergeSort {
         int nEsq = (meio+1)-esq, nDir = dir - meio, iEsq, iDir, i;
         int[] arrEsq = new int[nEsq+1];
         int[] arrDir = new int[nDir+1];
-        arrEsq[nEsq] = arrDir[nDir] = 0x7FFFFFFF;
+        arrEsq[nEsq] = arrDir[nDir] = 0x7FFFFFFF; // sentinel marks the end of each subarray (greatest possible number)
         for (iEsq = 0; iEsq < nEsq; iEsq++){
            arrEsq[iEsq] = arr[esq+iEsq];
         }
